45e98056e6b0a95369b4f2feb7746663a898b2b3
Fanomezana <hei.fanomezana.2@gmail.com>
2023-11-19 00:12:11 +0300

    set AuthorCrudOperations as repository

---
+++ src/main/java/Repository/AuthorCrudOperations.java
@@ -0,0 +1,82 @@
+package Repository;
+
+import Entity.Author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorCrudOperations implements CrudOperations<Author> {
+    private Connection connection;
+
+    public AuthorCrudOperations(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public List<Author> findAll() {
+        List<Author> authors = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM \"Authors\"")) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String sex = resultSet.getString("sex");
+                Author author = new Author(id, name, sex);
+                authors.add(author);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return authors;
+    }
+
+    @Override
+    public List<Author> saveAll(List<Author> toSave) {
+        List<Author> Authors = new ArrayList<>();
+        for (Author author : toSave) {
+            Author savedAuthor = save(author);
+            if (savedAuthor != null) {
+                Authors.add(savedAuthor);
+            }
+
+        }
+        return Authors;
+    }
+
+    @Override
+    public Author save(Author toSave) {
+        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO \"Author\" VALUES ?,?,?")) {
+            statement.setString(1, toSave.getId());
+            statement.setString(2, toSave.getName());
+            statement.setString(3, toSave.getSex());
+
+            int rowAffected = statement.executeUpdate();
+            if (rowAffected > 0) {
+                return toSave;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Author delete(Author toDelete) {
+        try(PreparedStatement statement= connection.prepareStatement("DELETE from\"Author\" where name ? ")) {
+            statement.setString(1, toDelete.getName());
+            int rowAffected = statement.executeUpdate();
+            if (rowAffected == 1) {
+                return toDelete;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } ;
+        return null;
+    }
+}
