b0d5956669982b48384dcef2e640ac2ece177ed1
Fanomezana <hei.fanomezana.2@gmail.com>
2023-11-18 11:01:21 +0300

    add script to create subscribers'table

--- src/main/java/DatabaseConfiguration/Parameters.java
+++
@@ -1,6 +0,0 @@
-package DatabaseConfiguration;
-
-public class Parameters {
-    public static String USERNAME = System.getenv("user");
-    public static String PASSWORD = System.getenv("password");
-}
---
+++ src/script postgresql/Table/createSubscribers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "subscribers"(
+id serial primary,
+nameSubscribers varchar (175),
+sex char(1) 
+);
+
