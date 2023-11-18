feac367d2d544e1b9dfd3ba9d182e3ed96d30956
Fanomezana <hei.fanomezana.2@gmail.com>
2023-11-19 02:04:25 +0300

    add script to insert Books

---
+++ src/script postgresql/Insert/insertBooks.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Books" (id, name, pageNumber, idAuthor, topic, status)
+VALUES
+    ('B001', 'The Great Gatsby', 180, 1, 'Romance', 'Available'),
+    ('B002', 'To Kill a Mockingbird', 281, 2, 'Other', 'Unavailable'),
+    ('B003', '1984', 328, 3, 'Other', 'Available');
