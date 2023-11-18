b2826ba25a3e8da6dde200327cde09899e65c9cf
Fanomezana <hei.fanomezana.2@gmail.com>
2023-11-18 09:19:46 +0300

    add class parameters to store parameters

---
+++ src/main/java/DatabaseConfiguration/Parameters.java
@@ -0,0 +1,6 @@
+package DatabaseConfiguration;
+
+public class Parameters {
+    public static String USERNAME = System.getenv("user");
+    public static String PASSWORD = System.getenv("password");
+}
