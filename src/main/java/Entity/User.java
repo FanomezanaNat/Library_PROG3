29e1884a80acf0c6508f56de68deac2ce3980be3
Fanomezana <hei.fanomezana.2@gmail.com>
2023-11-18 11:22:20 +0300

    set User as an entity

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,14 @@
+package Entity;
+
+import lombok.* ;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class User {
+    private String id ;
+    private String name;
+    private char sex;
+}
